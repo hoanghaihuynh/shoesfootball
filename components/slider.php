00089ac934be425f25bab51b18e563e2eed6ad57
hoanghaine <honghi.hunh@gmail.com>
2024-12-13 23:58:42 +0700

    Thêm: hình cho banner

--- components/slider.php
+++ components/slider.php
@@ -37,13 +37,13 @@ require_once('./config/database.php');
         <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active" data-bs-interval="10000">
-                    <img src="../img/sliders/pexels-gonzalo-acuna-166058093-10923070.jpg" class="d-block w-100" alt="...">
+                    <img src="../img/slides/pexels-gonzalo-acuna-166058093-10923070.jpg" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item" data-bs-interval="2000">
-                    <img src="../img/sliders/pexels-kampus-8941622.jpg" class="d-block w-100" alt="...">
+                    <img src="../img/slides/pexels-kampus-8941622.jpg" class="d-block w-100" alt="...">
                 </div>
                 <div class="carousel-item">
-                    <img src="../img/sliders/pexels-kevin-mccartney-254343-774321.jpg" class="d-block w-100" alt="...">
+                    <img src="../img/slides/pexels-kevin-mccartney-254343-774321.jpg" class="d-block w-100" alt="...">
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
