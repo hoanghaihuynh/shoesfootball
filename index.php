e960a86631ecbcd95009387e297e3d20b8b071bc
hoanghaine <honghi.hunh@gmail.com>
2024-12-15 21:10:34 +0700

    Feat (frontend): Thêm nút đăng nhập bằng Google

--- index.php
+++ index.php
@@ -10,28 +10,34 @@ if (isset($_SESSION["user"])) {
 
 <!DOCTYPE html>
 <html lang="vi">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Đăng ký và Đăng nhập</title>
     <link rel="stylesheet" href="./main.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+
     <style>
         * {
             padding: 0;
             margin: 0;
             box-sizing: border-box;
         }
+
         html {
             color: #333;
             font-size: 62.5%;
             font-family: 'Open Sans', sans-serif;
         }
+
         .main {
             background: #f1f1f1;
             min-height: 100vh;
             display: flex;
             justify-content: center;
         }
+
         .form {
             width: 360px;
             min-height: 100px;
@@ -43,9 +49,11 @@ if (isset($_SESSION["user"])) {
             align-self: center;
             box-shadow: 0 2px 5px 0 rgba(51, 62, 73, 0.1);
         }
+
         .form .heading {
             font-size: 2rem;
         }
+
         .form .desc {
             text-align: center;
             color: #636d77;
@@ -55,21 +63,25 @@ if (isset($_SESSION["user"])) {
             margin-top: 16px;
             font-weight: 300;
         }
+
         .form-group {
             display: flex;
             margin-bottom: 16px;
             flex-direction: column;
         }
+
         .form-label,
         .form-message {
             text-align: left;
         }
+
         .form-label {
             font-weight: 700;
             padding-bottom: 6px;
             line-height: 1.8rem;
             font-size: 1.4rem;
         }
+
         .form-control {
             height: 40px;
             padding: 8px 12px;
@@ -78,9 +90,11 @@ if (isset($_SESSION["user"])) {
             outline: none;
             font-size: 1.4rem;
         }
+
         .form-control:hover {
             border-color: #1dbfaf;
         }
+
         .form-submit {
             outline: none;
             background-color: #1dbfaf;
@@ -94,9 +108,49 @@ if (isset($_SESSION["user"])) {
             border-radius: 8px;
             cursor: pointer;
         }
+
+        .form-submit-google {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            outline: 1px solid #ccc;
+            background-color: #fff;
+            box-shadow: 0px 1px 2px 0px #00000008;
+            /* Màu nền Google */
+            padding: 12px 16px;
+            font-weight: 600;
+            color: #000;
+            border: none;
+            width: 100%;
+            font-size: 14px;
+            border-radius: 8px;
+            cursor: pointer;
+            transition: background-color 0.3s ease, transform 0.2s ease;
+        }
+
+        .form-submit-google img {
+            width: 24px;
+            height: 24px;
+            margin-right: 10px;
+        }
+
+        .form-submit-google i {
+            margin-right: 8px;
+            /* Khoảng cách giữa biểu tượng và chữ */
+            font-size: 1.2rem;
+        }
+
+        .form-submit-google:active {
+            background-color: #2a65c7;
+            /* Màu khi nhấn */
+            transform: translateY(0);
+            /* Trở lại vị trí gốc */
+        }
+
         .form-submit:hover {
             background-color: #1ac7b6;
         }
+
         .form-toggle {
             margin-top: 12px;
             font-size: 1.4rem;
@@ -104,69 +158,100 @@ if (isset($_SESSION["user"])) {
             cursor: pointer;
             text-decoration: underline;
         }
+
         .hidden {
             display: none;
         }
+
+        .divider {
+            display: flex;
+            align-items: center;
+            text-align: center;
+            margin: 10px 0;
+        }
+
+        .divider::before,
+        .divider::after {
+            content: '';
+            flex: 1;
+            border-bottom: 1px solid #ddd;
+        }
+
+        .divider::before {
+            margin-right: .25em;
+        }
+
+        .divider::after {
+            margin-left: .25em;
+        }
     </style>
 </head>
+
 <body>
-<div class="main">
-    <!-- Form đăng ký -->
-    <form action="" method="POST" class="form hidden" id="form-register">
-        <h3 class="heading">Thành viên đăng ký</h3>
-        <p class="desc">Đăng ký để có thể mua sắm những sản phẩm chất lượng! ❤️</p>
-        <div class="form-group">
-            <label for="fullname" class="form-label">Tên đầy đủ</label>
-            <input id="fullname" name="fullname" type="text" placeholder="VD: Hoàng Hậu" class="form-control" required>
-        </div>
-        <div class="form-group">
-            <label for="emailReg" class="form-label">Email</label>
-            <input id="emailReg" name="emailReg" type="email" placeholder="VD: email@domain.com" class="form-control" required>
-        </div>
-        <div class="form-group">
-            <label for="passReg" class="form-label">Mật khẩu</label>
-            <input id="passReg" name="passReg" type="password" placeholder="Nhập mật khẩu" class="form-control" required>
-        </div>
-        <div class="form-group">
-            <label for="confirmPassword" class="form-label">Nhập lại mật khẩu</label>
-            <input id="confirmPassword" name="confirmPassword" type="password" placeholder="Nhập lại mật khẩu" class="form-control" required>
-        </div>
-        <button class="form-submit" name="reg">Đăng ký</button>
-        <p class="form-toggle" onclick="toggleForm('login')">Đã có tài khoản? Đăng nhập</p>
-    </form>
+    <div class="main">
+        <!-- Form đăng ký -->
+        <form action="" method="POST" class="form hidden" id="form-register">
+            <h3 class="heading">Thành viên đăng ký</h3>
+            <p class="desc">Đăng ký để có thể mua sắm những sản phẩm chất lượng! ❤️</p>
+            <div class="form-group">
+                <label for="fullname" class="form-label">Tên đầy đủ</label>
+                <input id="fullname" name="fullname" type="text" placeholder="VD: Hoàng Hậu" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="emailReg" class="form-label">Email</label>
+                <input id="emailReg" name="emailReg" type="email" placeholder="VD: email@domain.com" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="passReg" class="form-label">Mật khẩu</label>
+                <input id="passReg" name="passReg" type="password" placeholder="Nhập mật khẩu" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="confirmPassword" class="form-label">Nhập lại mật khẩu</label>
+                <input id="confirmPassword" name="confirmPassword" type="password" placeholder="Nhập lại mật khẩu" class="form-control" required>
+            </div>
+            <button class="form-submit" name="reg">Đăng ký</button>
+            <p class="form-toggle" onclick="toggleForm('login')">Đã có tài khoản? Đăng nhập</p>
+        </form>
 
-    <!-- Form đăng nhập -->
-    <form action="" method="POST" class="form" id="form-login">
-        <h3 class="heading">Đăng nhập</h3>
-        <p class="desc">Cùng nhau mua sắm tại ShoesFootball! ❤️</p>
-        <div class="form-group">
-            <label for="email" class="form-label">Email</label>
-            <input id="email" name="email" type="email" placeholder="VD: email@domain.com" class="form-control" required>
-        </div>
-        <div class="form-group">
-            <label for="password" class="form-label">Mật khẩu</label>
-            <input id="password" name="password" type="password" placeholder="Nhập mật khẩu" class="form-control" required>
-        </div>
-        <button class="form-submit" name="login">Đăng nhập</button>
-        <p class="form-toggle" onclick="toggleForm('register')">Chưa có tài khoản? Đăng ký</p>
-    </form>
-</div>
+        <!-- Form đăng nhập -->
+        <form action="" method="POST" class="form" id="form-login">
+            <h3 class="heading">Đăng nhập</h3>
+            <p class="desc">Cùng nhau mua sắm tại ShoesFootball! ❤️</p>
+            <div class="form-group">
+                <label for="email" class="form-label">Email</label>
+                <input id="email" name="email" type="email" placeholder="VD: email@domain.com" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="password" class="form-label">Mật khẩu</label>
+                <input id="password" name="password" type="password" placeholder="Nhập mật khẩu" class="form-control" required>
+            </div>
+            <button class="form-submit" name="login">Đăng nhập</button>
+            <div class="divider">
+                or
+            </div>
+            <button class="form-submit-google" name="">
+                <img src="./img/icon/google_icon.webp" alt=""> Đăng nhập bằng Google
+            </button>
+            <p class="form-toggle" onclick="toggleForm('register')">Chưa có tài khoản? Đăng ký</p>
+        </form>
+    </div>
 
-<script>
-    function toggleForm(form) {
-        const registerForm = document.getElementById('form-register');
-        const loginForm = document.getElementById('form-login');
+    <script>
+        function toggleForm(form) {
+            const registerForm = document.getElementById('form-register');
+            const loginForm = document.getElementById('form-login');
 
-        if (form === 'login') {
-            registerForm.classList.add('hidden');
-            loginForm.classList.remove('hidden');
-        } else {
-            registerForm.classList.remove('hidden');
-            loginForm.classList.add('hidden');
+            if (form === 'login') {
+                registerForm.classList.add('hidden');
+                loginForm.classList.remove('hidden');
+            } else {
+                registerForm.classList.remove('hidden');
+                loginForm.classList.add('hidden');
+            }
         }
-    }
-</script>
+    </script>
 </body>
+
 </html>
 
 <?php
