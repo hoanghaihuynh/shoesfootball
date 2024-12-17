6fdba42b0bfafc9b989af86c71b597ab1824795d
hoanghaine <honghi.hunh@gmail.com>
2024-12-17 15:53:07 +0700

    nothing

--- index.php
+++ index.php
@@ -7,40 +7,8 @@ if (isset($_SESSION["user"])) {
     exit;
 }
 ?>
-<?php
 
-require_once 'vendor/autoload.php';
-
-// init configuration
-$clientID = '460428538946-l4ohfn4fml72n4v9tmiiq5etpfe6f97m.apps.googleusercontent.com';
-$clientSecret = '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';
-$redirectUri = 'http://localhost:8080/shoesfootball//home.php';
-
-// create Client Request to access Google API
-$client = new Google_Client();
-$client->setClientId($clientID);
-$client->setClientSecret($clientSecret);
-$client->setRedirectUri($redirectUri);
-$client->addScope("email");
-$client->addScope("profile");
-
-// authenticate code from Google OAuth Flow
-if (isset($_GET['code'])) {
-    $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
-    $client->setAccessToken($token['access_token']);
-
-    // get profile info
-    $google_oauth = new Google_Service_Oauth2($client);
-    $google_account_info = $google_oauth->userinfo->get();
-    $email =  $google_account_info->email;
-    $name =  $google_account_info->name;
-
-    $_SESSION["user"] = $email;
-    echo '<script>alert("Đăng nhập thành công!");window.location = "home.php";</script>';
-    // now you can use this profile info to create account in your website and make user logged in.
-}
-?>
-
+<!-- Paste dô đây -->
 
 <!DOCTYPE html>
 <html lang="vi">
