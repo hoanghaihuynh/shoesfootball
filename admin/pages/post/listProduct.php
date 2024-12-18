684c191c690a8bdc368476fa84bf40322b4c78f3
thanhhau <thanhhau@gmail.com>
2024-12-18 18:20:14 +0700

    Phan trang admin

--- admin/pages/post/listProduct.php
+++ admin/pages/post/listProduct.php
@@ -2,10 +2,22 @@
   require_once('../../../config/database.php');
   include_once('../header.php');
   include_once('../navigation.php');
- 
 
+  $records_per_page = 10; // Number of records per page
+  $page = isset($_GET['page']) ? intval($_GET['page']) : 1; // Current page
+  $offset = ($page - 1) * $records_per_page; // Calculate offset
+
+  // Get total number of records
+  $total_records_query = $conn->query("SELECT COUNT(*) as total FROM `products`");
+  $total_records = $total_records_query->fetch(PDO::FETCH_ASSOC)['total'];
+  $total_pages = ceil($total_records / $records_per_page);
+
+  // Get data for the current page
+  $query = $conn->query("SELECT * FROM `products` LIMIT $offset, $records_per_page");
+  $i = $offset + 1;
 ?>
-<div class="main-panel" style="">
+
+<div class="main-panel">
     <div class="row">
         <div class="col-md-12">
             <div class="card">
@@ -26,7 +38,6 @@
                                     <th>Tên Sản Phẩm</th>
                                     <th>Giá Chính</th>
                                     <th>Giá Cũ</th>
-
                                     <th>Danh Mục Sản Phẩm</th>
                                     <th>Xóa</th>
                                     <th>Sửa</th>
@@ -34,38 +45,22 @@
                             </thead>
                             <tbody>
                                 <?php 
-                            $i = 0 ;
-                                $query = $conn->query("SELECT * FROM `products`");
-                                while($row1 = $query->fetch(PDO::FETCH_ASSOC)){
-                                    //get list sản phẩm 
+                                while ($row1 = $query->fetch(PDO::FETCH_ASSOC)) {
                                 ?>
                                 <tr>
+                                    <td class="text-center"><?= $i++; ?></td>
                                     <td class="text-center">
-                                        <?= $i++; ?>
+                                        <img width="50" height="50" src="<?php echo $row1["image"]; ?>" alt="Product Image">
+                                    </td>
+                                    <td class="text-center"><?= $row1["nameProduct"]; ?></td>
+                                    <td class="text-center"><?= $row1["price"]; ?></td>
+                                    <td class="text-center"><?= $row1["giagoc"]; ?></td>
+                                    <td class="text-center"><?= $row1["danhmuc"]; ?></td>
+                                    <td class="text-center">
+                                        <a href='<?php echo $site_domain ?>admin/pages/post/delete.php?xoasp=<?php echo $row1["id"]; ?>' class="btn btn-danger">Xoá</a>
                                     </td>
                                     <td class="text-center">
-                                        <img width="50" height="50" src="<?php echo ''.$row1["image"]?>">
-                                    </td>
-                                    <td class="text-center" style="width:100px">
-                                        <?php echo ''.$row1["nameProduct"]?>
-                                    </td>
-                                    <td class="text-center">
-                                        <?php echo ''.$row1["price"]?>
-                                    </td>
-                                    <td class="text-center">
-                                        <?php echo ''.$row1["giagoc"]?>
-                                    </td>
-
-                                    <td class="text-center">
-                                        <?php echo ''.$row1["danhmuc"]?>
-                                    </td>
-                                    <td class="text-center">
-                                        <a href='<?php echo $site_domain?>/admin/pages/post/delete.php?xoasp=<?php echo ''.$row1["id"]?>'
-                                            class="btn btn-default">Xoá</a>
-                                    </td>
-                                    <td class="text-center">
-                                        <a href='<?php echo $site_domain?>/admin/pages/post/editProducts.php?updateid=<?php echo ''.$row1["id"]?>'
-                                            class="btn btn-default">Sửa</a>
+                                        <a href='<?php echo $site_domain ?>admin/pages/post/editProducts.php?updateid=<?php echo $row1["id"]; ?>' class="btn btn-warning">Sửa</a>
                                     </td>
                                 </tr>
                                 <?php } ?>
@@ -74,8 +69,37 @@
                     </div>
                 </div>
                 <!-- /.card-body -->
+
+                <!-- Pagination -->
                 <div class="card-footer clearfix">
-                    VUI LÒNG THAO TÁC CẨN THẬN
+                    <nav aria-label="Page navigation">
+                        <ul class="pagination justify-content-center">
+                            <!-- Previous Page Button -->
+                            <?php if ($page > 1): ?>
+                            <li class="page-item">
+                                <a class="page-link" href="?page=<?= $page - 1 ?>" aria-label="Previous">
+                                    <span aria-hidden="true">«</span>
+                                </a>
+                            </li>
+                            <?php endif; ?>
+
+                            <!-- Page Numbers -->
+                            <?php for ($i = 1; $i <= $total_pages; $i++): ?>
+                            <li class="page-item <?= ($i == $page) ? 'active' : '' ?>">
+                                <a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a>
+                            </li>
+                            <?php endfor; ?>
+
+                            <!-- Next Page Button -->
+                            <?php if ($page < $total_pages): ?>
+                            <li class="page-item">
+                                <a class="page-link" href="?page=<?= $page + 1 ?>" aria-label="Next">
+                                    <span aria-hidden="true">»</span>
+                                </a>
+                            </li>
+                            <?php endif; ?>
+                        </ul>
+                    </nav>
                 </div>
             </div>
             <!-- /.card -->
@@ -83,6 +107,9 @@
         <!-- /.col -->
     </div>
 </div>
+
+<?php include_once('../footer.php'); ?>
+
 
 <!-- main-panel ends -->
 </div>
